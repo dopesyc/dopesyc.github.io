fd4f757e0dbc9c5df04fcb8b7b10631e7c4fbfd1
JJUCIAL <sleep3rp@gmail.com>
2025-03-14 03:34:50 -0500

    silverfox outfitters test push

--- assets/addressbar-color.ccb914c8.js
+++
@@ -1,1 +0,0 @@
-import{A as o}from"./index.30682bb0.js";var a=()=>{console.log("color loaded"),o.set("#a2e3fa")};export{a as default};
--- assets/pinia.120d0a0f.js
+++
@@ -1,1 +0,0 @@
-import{c as r}from"./pinia.b03a0949.js";import"./index.30682bb0.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
