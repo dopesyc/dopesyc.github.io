37f1f3cf6405e3c7b09b49ca5f10461ea4b0299f
JJUCIAL <sleep3rp@gmail.com>
2025-03-16 12:15:10 -0500

    favtest

--- assets/addressbar-color.945f3252.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./index.c893d867.js";var o=()=>{r.set("#a2e3fa")};export{o as default};
---
+++ assets/pinia.c08765e8.js
@@ -0,0 +1,1 @@
+import{c as r}from"./pinia.a3ff7ac6.js";import"./index.c856f496.js";var o=({app:a})=>{const i=r();a.use(i)};export{o as default};
