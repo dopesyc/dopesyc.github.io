4d5acdeb4accdd537869407aca3d43821ee72182
JJUCIAL <sleep3rp@gmail.com>
2025-03-17 02:51:06 -0500

    test push

--- assets/addressbar-color.37360edd.js
+++
@@ -1,1 +0,0 @@
-import{A as r}from"./index.eca8921f.js";var o=()=>{r.set("#a2e3fa")};export{o as default};
---
+++ assets/addressbar-color.a777ee8d.js
@@ -0,0 +1,1 @@
+import{A as r}from"./index.5d480950.js";var o=()=>{r.set("#a2e3fa")};export{o as default};
