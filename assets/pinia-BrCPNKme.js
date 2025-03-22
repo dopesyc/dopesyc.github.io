908c528a6f4f34be26b3838380bdfd545d273f2a
JJUCIAL <sleep3rp@gmail.com>
2025-03-21 21:53:35 -0500

    ui test push

--- assets/pinia-BrCPNKme.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./index-2y3zcXbB.js";const e=({app:o})=>{console.log("[Boot] Registering Pinia"),o.use(i())};export{e as default};
---
+++ assets/pinia-CB-ifMHz.js
@@ -0,0 +1,1 @@
+import{c as i}from"./index-D0iQehBi.js";const e=({app:o})=>{console.log("[Boot] Registering Pinia"),o.use(i())};export{e as default};
