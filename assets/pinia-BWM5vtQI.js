0789e2ff5a546272f0fde16260fc5b20b9b11cd4
JJUCIAL <sleep3rp@gmail.com>
2025-03-23 03:22:12 -0500

    test push to fix bug

---
+++ assets/pinia-BWM5vtQI.js
@@ -0,0 +1,1 @@
+import{c as i}from"./index-DPes5Osq.js";const e=({app:o})=>{console.log("[Boot] Registering Pinia"),o.use(i())};export{e as default};
--- assets/pinia-BrVIAuq1.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./index-Dy-DK_hN.js";const e=({app:o})=>{console.log("[Boot] Registering Pinia"),o.use(i())};export{e as default};
